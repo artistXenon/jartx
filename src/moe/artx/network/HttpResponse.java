f7e8e385ba99c92fc2c0523bfdc56904b387ea4b
artistXenon <xtramainpp@gmail.com>
2021-05-04 19:06:01 +0900

    init as gradle project

--- src/moe/artx/network/HttpResponse.java
+++
@@ -1,9 +0,0 @@
-package moe.artx.network;
-
-import java.net.HttpURLConnection;
-
-public interface HttpResponse {
-    void onResponse(HttpURLConnection connection) throws Exception;
-
-    void onException(Exception e);
-}
